da64e0d76bea0cf9d079b25d289c8d020c8c0576
Muhammad Naufal Ramadhan <naufalnauf04@gmail.com>
2025-03-07 18:43:19 +0700

    Add Repository Decorator in Payment Repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryImpl.java
@@ -1,12 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@Repository
 public class PaymentRepositoryImpl implements PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
