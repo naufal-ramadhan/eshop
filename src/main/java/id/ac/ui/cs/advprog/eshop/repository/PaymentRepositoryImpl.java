a546d5fbdde01284a4b61c6ea9d4a51c87892d84
Muhammad Naufal Ramadhan <naufalnauf04@gmail.com>
2025-03-07 16:18:33 +0700

    [GREEN] Implement PaymentRepository class.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryImpl.java
@@ -5,17 +5,42 @@ import id.ac.ui.cs.advprog.eshop.model.Payment;
 import java.util.ArrayList;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class PaymentRepositoryImpl implements PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
-    public Payment create(final Payment payment) {return null;}
+    public Payment create(final Payment payment) {
+        paymentData.add(payment);
+        return payment;
+    }
 
-    public Payment findById(final String paymentId) {return null;}
+    public Payment findById(final String paymentId) {
+        return paymentData.stream()
+                .filter(payment -> payment.getId().equals(paymentId)).findFirst().orElse(null);
+    }
 
-    public Boolean existById(final String productId) {return null;}
+    public Boolean existById(final String productId) {
+        return paymentData.stream().anyMatch(product -> product.getId().equals(productId));
+    }
 
-    public Payment delete(final Payment payment) {return null;}
+    public Payment update(final Payment payment) {
+        Payment old = findById(payment.getId());
+        if (old == null){
+            throw new NoSuchElementException("Payment not found");
+        }
 
-    public List<Payment> findAll() {return null;}
+        this.paymentData.remove(old);
+        this.paymentData.add(payment);
+        return payment;
+    }
+
+    public Payment delete(final Payment payment) {
+        paymentData.remove(payment);
+        return payment;
+    }
+
+    public List<Payment> findAll() {
+        return new ArrayList<>(paymentData);
+    }
 }
